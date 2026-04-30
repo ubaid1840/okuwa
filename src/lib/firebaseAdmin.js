c8bef4db9256856cb5eb14fffa0ad9d2ed8c7e34
ubaid1840 <ubaidrehman1840@gmail.com>
2026-04-30 10:56:49 +0500

    firebase updated

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,10 +1,13 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = process.env.SERVICE_KEY_ACCOUNT; 
+const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64 || "";
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
+
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert(serviceAccount),
+        storageBucket : "adcrm-a0bf3.appspot.com"
     });
 }
 
